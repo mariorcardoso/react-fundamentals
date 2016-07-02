2f488291d54d433e24286df9b081ad195f081672
Mario Cardoso <cardosomarioar@gmail.com>
2016-07-02 16:44:24 +0100

    Hello World - First Component

---
+++ hello_world-first_component/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// Class component can have state
+// class App extends React.Component {
+//   render(){
+//     return <h1>Hello Guys</h1>
+//   }
+// }
+
+// Stateless component
+const App = () => <h1>Hello Eggheads</h1>
+
+export default App
